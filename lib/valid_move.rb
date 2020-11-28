ac338fb206afea821c14a26ee43cce1b6c45693c
remote-torrent-6747 <abbyc@sky.com>
2020-11-28 13:03:46 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,15 @@
-def valid_move? (board, index)
-    if index > 8
-      return false
-    elsif board[index] == "X" || board[index] == "O"
-      return false
-    elsif board[index] == " " || board[index] == "" || board[index] == nil
-      return true
+def valid_move?(board, index)
+  if index > 8 || index < 0 || position_taken(board, index)
+    return false
+  else
+    return true
+  end
+end
+
+def position_taken?(board, index)
+  if board[index] == "X" || board[index] == "O"
+    return true
+  else
+    return false
   end
 end
